e6f0da911064d5573b834ebb2056c7bd83b17c4e
Brad-Davis <bcd296@nyu.edu>
2021-03-14 20:20:22 -0400

    gutted the store probably wont use

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,8 @@ import React, {useContext} from 'react'
 import {Responsive} from 'semantic-ui-react'
 import DesktopMenu from './DesktopMenu'
 import MobileMenu from './MobileMenu'
-import AuthContext from '../Context/AuthContext'
-import CartContext from '../Context/CartContext'
 
 const Header = ({location}) => {
-  const {cartCount} = useContext(CartContext)
-  const {token, signOut} = useContext(AuthContext)
 
   return (
     <>
