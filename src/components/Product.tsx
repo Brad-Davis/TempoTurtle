33fd447db82a54dd337cd747ede1b2757f1f5900
Brad Davis <braddavis97@gmail.com>
2021-04-19 17:48:04 -0400

    photo fix

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -12,23 +12,23 @@ export default class Product extends Component {
                 </div>
                 <div className="row container">
                     <div className="col threeColTitle">
-                        human <br/> centric <br/> design
+                        human <br /> centric <br /> design
                     </div>
                     <div className="col threeColTitle">
-                        no music <br/> theory <br/> needed
+                        no music <br /> theory <br /> needed
                     </div>
                     <div className="col threeColTitle">
-                        jam <br/> away <br/> anywhere
+                        jam <br /> away <br /> anywhere
                     </div>
                 </div>
-                <div style={{paddingTop: '3em'}} className="container">
-                    <div style={{alignItems: 'center', textAlign: 'center'}} className="row">
-                        <div className="col-6"><img className="photoimg" src="./bradpad_photos/2.jpg"/></div>
+                <div style={{ paddingTop: '3em' }} className="container">
+                    <div style={{ alignItems: 'center', textAlign: 'center' }} className="row">
+                        <div className="col-6"><img className="photoimg" src="./2.jpg" /></div>
                         <div className="col-6 twoColTitle">A MIDI controller<br />for the masses</div>
                     </div>
-                    <div style={{alignItems: 'center', textAlign: 'center'}} className="row">
+                    <div style={{ alignItems: 'center', textAlign: 'center' }} className="row">
                         <div className="col-6 twoColTitle">Customize your<br />look and feel</div>
-                        <div className="col-6"><img className="photoimg" src="./bradpad_photos/1.jpg"/></div>
+                        <div className="col-6"><img className="photoimg" src="./1.jpg" /></div>
                     </div>
                 </div>
             </div>
