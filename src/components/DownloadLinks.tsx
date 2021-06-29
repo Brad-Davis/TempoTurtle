3bfc93ae4cb3ba227d2ca67ba628b987648ccae4
wxyangf <2058629+wxyangf@users.noreply.github.com>
2021-06-29 00:32:44 -0400

    added link to via, and to the source files

--- src/components/DownloadLinks.tsx
+++ src/components/DownloadLinks.tsx
@@ -15,6 +15,8 @@ export default class DownloadLinks extends Component<DownloadProps, DownloadStat
                 <a href="https://tempoturtle.com/firmware/tempo_turtle_bradpad_default.hex">Default Firmware</a>
                 <a href="https://tempoturtle.com/firmware/tempo_turtle_bradpad_midi_demo.hex ">MIDI Demo </a>
                 <a href="https://tempoturtle.com/firmware/tempo_turtle_bradpad_4brad.hex">The MIDI firmware Brad uses</a>
+                <a href="https://tempoturtle.com/firmware/tempo_turtle_bradpad_via.hex">VIA firmware</a>
+                <a href="https://github.com/qmk/qmk_firmware/tree/master/keyboards/tempo_turtle">Source Code on Github</a>
             </div>
         )
     }
